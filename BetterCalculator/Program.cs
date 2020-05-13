bd4b511e19ffbc8634a6cb802a3fd3c327cefe1e
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-05-13 15:43:48 +0100

    initial commit

---
+++ BetterCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
